7af193be882bdc2264acbdf98fd16c757f908d51
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2025-04-10 17:06:52 +0530

    feat: implement length

---
+++ java/app/src/main/java/com/technogise/foundation/geometry/Line.java
@@ -0,0 +1,18 @@
+package com.technogise.foundation.geometry;
+
+public record Line(Point a, Point b) {
+    public double run() {
+        return b.x() - a.x();
+    }
+
+    public double rise() {
+        return b.y() - a.y();
+    }
+
+    public double length() {
+        double dx = run();
+        double dy = rise();
+
+        return Math.sqrt(dx*dx + dy*dy);
+    }
+}
