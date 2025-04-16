1e7915c4de26712a7f33ffab152192eafd5b292d
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2025-04-16 17:10:08 +0530

    test: add failing test to calculate rise of a line

---
+++ rust/src/geometry/line.rs
@@ -0,0 +1,6 @@
+use super::point::Point;
+
+pub struct Line {
+    pub start: Point,
+    pub end: Point,
+}
---
+++ rust/tests/geometry/line.rs
@@ -0,0 +1,10 @@
+use crate::geometry::{line::Line, point::Point};
+
+#[test]
+fn test_line_calculate_rise() {
+    let start = Point { x: 1.0, y: 2.0 };
+    let end = Point { x: 3.0, y: 5.0 };
+    let line = Line { start, end };
+    
+    assert_eq!(line.rise(), 3.0);
+}
