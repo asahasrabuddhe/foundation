a6af7924b9a6f11ba0cc4ac14f1eecbc24063750
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2025-04-16 17:11:07 +0530

    feat: implement run calculation for Line

--- rust/tests/geometry/line.rs
+++ rust/tests/geometry/line.rs
@@ -8,3 +8,12 @@ fn test_line_calculate_rise() {
     
     assert_eq!(line.rise(), 3.0);
 }
+
+#[test]
+fn test_line_calculate_run() {
+    let start = Point { x: 1.0, y: 2.0 };
+    let end = Point { x: 3.0, y: 5.0 };
+    let line = Line { start, end };
+    
+    assert_eq!(line.run(), 2.0);
+}
