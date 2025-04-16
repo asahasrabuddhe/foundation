82b2a4a22be01ce94c7e91883a30236fd6b5572b
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2025-04-16 15:21:36 +0530

    fix: update test to ensure perpendicular lines have slopes that are negative reciprocals

--- go/geometry/line_test.go
+++ go/geometry/line_test.go
@@ -79,12 +79,12 @@ func TestLine_IsParallel(t *testing.T) {
 func TestLine_IsPerpendicular(t *testing.T) {
 	// Test that perpendicular lines have slopes which are negative reciprocals
 
-	p1 := Point{X: 1.0, Y: 2.0}
-	p2 := Point{X: 3.0, Y: 5.0}
+	p1 := Point{X: 0.0, Y: 0.0}
+	p2 := Point{X: 2.0, Y: 2.0}
 	l1 := Line{Start: p1, End: p2}
 
-	p3 := Point{X: 2.0, Y: 3.0}
-	p4 := Point{X: 4.0, Y: 1.0}
+	p3 := Point{X: 0.0, Y: 0.0}
+	p4 := Point{X: -2.0, Y: 2.0}
 	l2 := Line{Start: p3, End: p4}
 
 	if !l1.IsPerpendicularTo(l2) {
