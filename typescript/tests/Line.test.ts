257d7faa653d19817aac8ba6556b7f42261f9e0d
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2025-04-16 14:04:44 +0530

    test: add test to verify slope calculation of a line

--- typescript/tests/Line.test.ts
+++ typescript/tests/Line.test.ts
@@ -28,3 +28,12 @@ test('Length of the line is calculated correctly', () => {
     
     expect(line.length).toBe(5);
 });
+
+test('Slope of the line is calculated correctly', () => {
+    const a = new Point(0, 0);
+    const b = new Point(3, 4);
+
+    const line = new Line(a, b);
+    
+    expect(line.slope()).toBe(4 / 3);
+});
