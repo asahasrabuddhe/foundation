f0ae8341070261c4c6da93ed3a1dc6768143de99
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2025-04-16 13:48:08 +0530

    test: add test to verify length calculation of a line

--- typescript/tests/Line.test.ts
+++ typescript/tests/Line.test.ts
@@ -19,3 +19,12 @@ test('Run of the line is calculated correctly', () => {
     
     expect(line.run).toBe(3);
 });
+
+test('Length of the line is calculated correctly', () => {
+    const a = new Point(0, 0);
+    const b = new Point(3, 4);
+
+    const line = new Line(a, b);
+    
+    expect(line.length).toBe(5);
+});
