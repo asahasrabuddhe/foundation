46f6b03168e6c4d3ef974f522ba8403cc862134a
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2025-04-16 14:20:36 +0530

    chore: implement main to demonstrate the use of line / point

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+import { Point } from "./src/Point";
+import { Line } from "./src/Line";
+
+const a = new Point(1, 1);
+const b = new Point(4, 5);
+
+const line = new Line(a, b);
+
+console.log((`Length of the line is: ${line.length}`));
+
+const x = new Point(0, 0);
+const y = new Point(3, 4);
+
+const line2 = new Line(x, y);
+line2.isParallelTo(line) ? console.log("line and line2 are parallel") : console.log("line and line2 are not parallel");
+
+const p = new Point(0, 0);
+const q = new Point(-4, 3);
+
+const line3 = new Line(p, q);
+line3.isPerpendicularTo(line) ? console.log("line and line3 are perpendicular") : console.log("line and line3 are not perpendicular");
+
