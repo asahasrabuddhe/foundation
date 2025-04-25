1e77d112666b2c2109f99b81e632db3f2e914553
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2025-04-25 10:58:06 +0530

    test: add failing test to implement DivideOperation

---
+++ calculator/java/app/untitled/src/test/java/com/technogise/foundation/DivideOperationTest.java
@@ -0,0 +1,24 @@
+package com.technogise.foundation;
+
+import com.technogise.foundation.calculator.DivideOperation;
+import com.technogise.foundation.calculator.exception.DivideByZeroException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DivideOperationTest {
+    @Test
+    void testDivideOperation() {
+        DivideOperation divideOperation = new DivideOperation();
+        assertEquals("*", divideOperation.getSymbol());
+        assertEquals(12.0, divideOperation.apply(6.0, 2.0));
+    }
+
+    @Test
+    void testDivideByZeroOperationThrowsErrors() {
+        DivideOperation divideOperation = new DivideOperation();
+        assertEquals("*", divideOperation.getSymbol());
+        assertThrows(DivideByZeroException.class, () -> divideOperation.apply(6.0, 0.0));
+    }
+}
