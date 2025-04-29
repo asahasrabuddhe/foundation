f104108c38efe42e006a2899fcb4621f33e68898
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2025-04-29 15:37:26 +0530

    feat: support brackets in the Tokenizer

--- calculator/java/app/src/main/java/com/technogise/foundation/parser/lexer/Tokenizer.java
+++ calculator/java/app/src/main/java/com/technogise/foundation/parser/lexer/Tokenizer.java
@@ -5,6 +5,6 @@ import java.util.List;
 
 public class Tokenizer  {
     public static List<String> tokenize(String expression) {
-        return Arrays.asList(expression.replaceAll("([+\\-*])", " $1 ").trim().split("\\s+"));
+        return Arrays.asList(expression.replaceAll("([+\\-*/()])", " $1 ").trim().split("\\s+"));
     }
 }
