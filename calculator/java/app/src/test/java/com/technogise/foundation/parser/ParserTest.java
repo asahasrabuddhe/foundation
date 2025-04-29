b2144d82ed205792391bb7a274d9e8302357f058
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2025-04-29 15:19:26 +0530

    test: add failing test to validate precedence handling in the Parser

--- calculator/java/app/src/test/java/com/technogise/foundation/parser/ParserTest.java
+++ calculator/java/app/src/test/java/com/technogise/foundation/parser/ParserTest.java
@@ -3,7 +3,9 @@ package com.technogise.foundation.parser;
 import com.technogise.foundation.core.OperationRegistry;
 import com.technogise.foundation.core.Operation;
 import com.technogise.foundation.operations.AddOperation;
+import com.technogise.foundation.operations.DivideOperation;
 import com.technogise.foundation.operations.MultiplyOperation;
+import com.technogise.foundation.operations.SubtractOperation;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -34,6 +36,35 @@ public class ParserTest {
         List<String> tokens = List.of("12.0");
         List<String> result = Parser.toPostfix(tokens, registryWith());
         List<String> postfix = List.of("12.0");
+
+        assertEquals(postfix, result);
+    }
+
+    @Test
+    void shouldRespectOperatorPrecedenceMultiplicationOverAddition() {
+        List<String> tokens = List.of("1", "+", "2", "*", "3"); // 1 + 2 * 3
+        List<String> result = Parser.toPostfix(tokens, registryWith(new AddOperation(), new MultiplyOperation()));
+        List<String> postfix = List.of("1", "2", "3", "*", "+");
+
+        assertEquals(postfix, result);
+    }
+
+    @Test
+    void shouldRespectEqualPrecedenceLeftToRightForMultiplicationAndDivision() {
+        List<String> tokens = List.of("6", "/", "3", "*", "2");
+        List<String> result = Parser.toPostfix(tokens, registryWith(
+                new MultiplyOperation(), new DivideOperation()));
+        List<String> postfix = List.of("6", "3", "/", "2", "*"); // Left to right
+
+        assertEquals(postfix, result);
+    }
+
+    @Test
+    void shouldRespectEqualPrecedenceLeftToRightForAddAndSubtract() {
+        List<String> tokens = List.of("5", "+", "4", "-", "3");
+        List<String> result = Parser.toPostfix(tokens, registryWith(
+                new AddOperation(), new SubtractOperation()));
+        List<String> postfix = List.of("5", "4", "+", "3", "-");
 
         assertEquals(postfix, result);
     }
