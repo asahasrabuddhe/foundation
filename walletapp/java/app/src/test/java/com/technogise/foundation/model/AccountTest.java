1468baee0713d43426f067327b882e99eff07d57
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2025-05-04 16:01:57 +0530

    test: add failing tests for credit / debit operations

--- walletapp/java/app/src/test/java/com/technogise/foundation/model/AccountTest.java
+++ walletapp/java/app/src/test/java/com/technogise/foundation/model/AccountTest.java
@@ -10,4 +10,19 @@ public class AccountTest {
         Account account = new Account();
         assertEquals(0, account.getBalance());
     }
+
+    @Test
+    void accountShouldBeCreditedCorrectly() {
+        Account account = new Account();
+        account.credit(100);
+        assertEquals(100, account.getBalance());
+    }
+
+    @Test
+    void accountShouldBeDebitedCorrectly() {
+        Account account = new Account();
+        account.credit(100);
+        account.debit(30);
+        assertEquals(70, account.getBalance());
+    }
 }
