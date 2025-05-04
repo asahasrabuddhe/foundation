bd2485afa44a52a769a130cc5d908361b8c951ef
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2025-05-04 16:01:57 +0530

    feat: implement credit and debit for Account

--- walletapp/java/app/src/main/java/com/technogise/foundation/model/Account.java
+++ walletapp/java/app/src/main/java/com/technogise/foundation/model/Account.java
@@ -1,13 +1,21 @@
 package com.technogise.foundation.model;
 
 public class Account {
-    private int balance;
+    private double balance;
 
     public Account() {
         balance = 0;
     }
 
-    public int getBalance() {
+    public double getBalance() {
         return balance;
+    }
+
+    public void credit(double amount) {
+        balance += amount;
+    }
+
+    public void debit(double amount) {
+        balance -= amount;
     }
 }
