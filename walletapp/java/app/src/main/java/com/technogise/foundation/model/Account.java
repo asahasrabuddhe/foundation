2e0347dafca6117607b089d284d2ff56e804505f
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2025-05-04 15:59:41 +0530

    feat: implement new Account with zero balance.

---
+++ walletapp/java/app/src/main/java/com/technogise/foundation/model/Account.java
@@ -0,0 +1,13 @@
+package com.technogise.foundation.model;
+
+public class Account {
+    private int balance;
+
+    public Account() {
+        balance = 0;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
