3ef74bed9dbe50539b76107caaa2407db00627ee
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2025-05-05 21:34:20 +0530

    test: add failing tests to validate that transfers are done correctly

--- walletapp/java/app/src/test/java/com/technogise/foundation/service/WalletServiceTest.java
+++ walletapp/java/app/src/test/java/com/technogise/foundation/service/WalletServiceTest.java
@@ -1,6 +1,7 @@
 package com.technogise.foundation.service;
 
 import com.technogise.foundation.exceptions.DuplicateUserException;
+import com.technogise.foundation.exceptions.InsufficientBalanceException;
 import com.technogise.foundation.exceptions.UserDoesNotExistException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -37,5 +38,25 @@ public class WalletServiceTest {
     @Test
     void shouldTopUpBalanceOnlyIfTheUserExists() {
         assertThrows(UserDoesNotExistException.class, () -> walletService.topUp("alice", 100));
+    }
+
+    @Test
+    void shouldTransferMoneyCorrectly() {
+        walletService.registerUser("alice");
+        walletService.registerUser("bob");
+        walletService.topUp("alice", 100);
+        walletService.transfer("alice", "bob", 50);
+        assertEquals(50, walletService.getBalance("alice"));
+        assertEquals(50, walletService.getBalance("bob"));
+    }
+
+    @Test
+    void shouldFailTransferIfInsufficientBalance() {
+        walletService.registerUser("alice");
+        walletService.registerUser("bob");
+        assertThrows(
+                InsufficientBalanceException.class,
+                () -> walletService.transfer("alice", "bob", 100)
+        );
     }
 }
