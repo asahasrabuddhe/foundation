7febec447d3c8946a57540be218a9452991475aa
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2025-05-05 21:34:20 +0530

    feat: implement transfer transactions correctly

--- walletapp/java/app/src/main/java/com/technogise/foundation/service/WalletService.java
+++ walletapp/java/app/src/main/java/com/technogise/foundation/service/WalletService.java
@@ -2,6 +2,7 @@ package com.technogise.foundation.service;
 
 import com.technogise.foundation.exceptions.DuplicateUserException;
 import com.technogise.foundation.exceptions.UserDoesNotExistException;
+import com.technogise.foundation.model.Transaction;
 import com.technogise.foundation.model.User;
 
 import java.util.HashMap;
@@ -19,25 +20,45 @@ public class WalletService implements IWalletService {
     @Override
     public void registerUser(String username) {
         if (users.containsKey(username)) {
-            throw new DuplicateUserException("User already exists");
+            throw new DuplicateUserException("User " + username + " already exists");
         }
         users.put(username, new User(username));
     }
 
     @Override
     public void topUp(String username, double amount) {
-        if (!users.containsKey(username)) {
-            throw new UserDoesNotExistException("User " + username + " does not exist");
-        }
-        users.get(username).getAccount().credit(amount);
+        validateUser(username);
+
+        User user = users.get(username);
+
+        user.getAccount().credit(amount);
+
+        transactionHistoryService.recordTransaction(Transaction.topUp(user, amount));
     }
 
+    @Override
     public void transfer(String fromUsername, String toUsername, double amount) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        validateUser(fromUsername);
+        validateUser(toUsername);
+
+        User sender = users.get(fromUsername);
+        User receiver = users.get(toUsername);
+
+        sender.getAccount().debit(amount);
+        receiver.getAccount().credit(amount);
+
+        transactionHistoryService.recordTransaction(Transaction.transfer(sender, receiver, amount));
     }
 
     @Override
     public double getBalance(String username) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        validateUser(username);
+        return users.get(username).getAccount().getBalance();
+    }
+
+    private void validateUser(String username) {
+        if (!users.containsKey(username)) {
+            throw new UserDoesNotExistException("User " + username + " does not exist");
+        }
     }
 }
