ba72724d1d4a4700ba66ba15c38e01e102409a32
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2025-05-06 16:15:01 +0530

    feat: implement transfer command

---
+++ walletapp/java/app/src/main/java/com/technogise/foundation/cli/command/TransferCommand.java
@@ -0,0 +1,35 @@
+package com.technogise.foundation.cli.command;
+
+import com.technogise.foundation.service.MoneyOps;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class TransferCommand implements CommandStrategy {
+    private final MoneyOps moneyOps;
+    private final Scanner in;
+    private final PrintStream out;
+
+    public TransferCommand(MoneyOps moneyOps, Scanner in, PrintStream out) {
+        this.moneyOps = moneyOps;
+        this.in = in;
+        this.out = out;
+    }
+
+    @Override
+    public void execute() {
+        out.print("Enter your username: ");
+        String from = in.nextLine();
+        out.print("Enter recipient's username: ");
+        String to = in.nextLine();
+        out.print("Enter amount: ");
+        double amount = Double.parseDouble(in.nextLine());
+        moneyOps.transfer(from, to, amount);
+        out.println("Transfer successful.");
+    }
+
+    @Override
+    public String getDescription() {
+        return "Transfer money to another user";
+    }
+}
