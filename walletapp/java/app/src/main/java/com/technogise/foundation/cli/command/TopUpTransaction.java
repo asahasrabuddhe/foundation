f9a3ad3d16d2fc5e1024b2e0465bf0e0724ed4ad
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2025-05-06 16:04:37 +0530

    feat: implement topup command

---
+++ walletapp/java/app/src/main/java/com/technogise/foundation/cli/command/TopUpTransaction.java
@@ -0,0 +1,33 @@
+package com.technogise.foundation.cli.command;
+
+import com.technogise.foundation.service.MoneyOps;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class TopUpTransaction implements CommandStrategy{
+    private final MoneyOps moneyOps;
+    private final Scanner in;
+    private final PrintStream out;
+
+    public TopUpTransaction(MoneyOps moneyOps, Scanner in, PrintStream out) {
+        this.moneyOps = moneyOps;
+        this.in = in;
+        this.out = out;
+    }
+
+    @Override
+    public void execute() {
+        out.print("Enter username: ");
+        String username = in.nextLine();
+        out.print("Enter amount: ");
+        double amount = Double.parseDouble(in.nextLine());
+        moneyOps.topUp(username, amount);
+        out.println("Top-up successful.");
+    }
+
+    @Override
+    public String getDescription() {
+        return "Top up money into your wallet";
+    }
+}
